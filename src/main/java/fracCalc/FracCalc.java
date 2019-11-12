b5a970059f03e4578f27b574f1d1a672276bf124
s-shirkh <s-shirkh@53S50030844.it.bsd405.org>
2019-11-12 12:29:56 -0800

    fraccalc checkpoint 1

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -3,12 +3,16 @@
  */
 
 package fracCalc;
-
+import java.util.Scanner;
 public class FracCalc {
 
     public static void main(String[] args)
     {
-        // TODO: Read the input from the user and call produceAnswer with an equation
+    	Scanner userInput = new Scanner (System.in);
+        System.out.println("Hi! Welcome to the FracCalc!");
+        System.out.print("Please enter a fraction problem: ");
+        String userResponse = userInput.nextLine();
+        System.out.println(produceAnswer(userResponse));
 
     }
 
@@ -20,11 +24,22 @@ public class FracCalc {
     //
     // The function should return the result of the fraction after it has been calculated
     //      e.g. return ==> "1_1/4"
-    public static String produceAnswer(String input)
-    {
-        // TODO: Implement this function to produce the solution to the input
-
-        return "";
+    public static String produceAnswer(String input) {
+       
+    	String temporary = input;
+    	String operand1 = temporary.substring(0, temporary.indexOf(' '));
+    	temporary = temporary.substring(temporary.indexOf(' ') + 1);
+    	String operator = temporary.substring(0, temporary.indexOf(' '));
+    	temporary = temporary.substring(temporary.indexOf(' ') + 1);
+    	String operand2 = temporary;
+    	
+    	
+    	 /*Scanner userInput = new Scanner(input);
+    	String operand1 = userInput.next();
+    	String operator = userInput.next();
+    	String operand2 = userInput.next(); **/ 
+    	
+        return operand2;
     }
 
     // TODO: Fill in the space below with any helper methods that you think you will need
